02e501edfcb79bea4c080f0292f4140e2bb9aba5
Nick McCready <nmccready@gmail.com>
2015-10-29 17:37:54 -0400

    chore(build, changelog): modifying changelog behavior to be removed from bump

--- grunt/changelog.js
+++ grunt/changelog.js
@@ -2,16 +2,25 @@
 var _ = require('lodash'),
     _pkg = require('../package.json'),
     last = _.last(_pkg.version.split('.')),
-    next = Number(last) + 1;
+    next = Number(last) + 1,
+    argv = require('yargs').argv;
 
-// note this will fail on new minor or major releases.. oh well manually fix it
-// for now as this is mainly for changelog
+/*
+_pkg.nextVersion only works for patch updates
+
+Using changelog in the workflow is not really recommended right now as changelog is not really able to be modified until
+a new tag is released.
+
+Using Yargs as a workaround which grunt should be used in this way:
+
+`grunt changelog --ui_leaflet_ver SOME_TAG_NUMBER`
+*/
 _pkg.nextVersion = _pkg.version.replace(last, String(next));
 
 module.exports = function (grunt, options) {
     return {
         options:{
-            version: _pkg.nextVersion
+            version: argv.ui_leaflet_ver || _pkg.nextVersion
         }
     };
 };

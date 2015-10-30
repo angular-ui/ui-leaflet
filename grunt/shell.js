ef7d702ead182d2c32b8380291b910712abf6ca1
Nick McCready <nmccready@gmail.com>
2015-10-29 22:55:44 -0400

    revert grunt examples

--- grunt/shell.js
+++ grunt/shell.js
@@ -17,10 +17,9 @@ module.exports = function(grunt, options) {
         },
         npm_install: {
             command: 'npm install'
+        },
+        examples: {
+            command: 'node generate-examples.js'
         }
-        // ,
-        // examples: {
-        //     command: 'node generate-examples.js'
-        // }
     };
 };

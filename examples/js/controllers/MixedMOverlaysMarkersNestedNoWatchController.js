67c186d81b85d68d94504660e8039893c9094c27
Michael A. Salgado <michaelsalgado@gkudos.com>
2015-12-01 17:55:46 -0500

    fix(examples) fix MapboxGL example

--- examples/js/controllers/MixedMOverlaysMarkersNestedNoWatchController.js
+++ examples/js/controllers/MixedMOverlaysMarkersNestedNoWatchController.js
@@ -18,12 +18,12 @@
             });
         }, 4000);
         angular.extend($scope, {
-            watchOptions: {
-                markers: {
-                    type: null
-                    individual: {
-                        type: null
-                    }
+            markersWatchOptions: {
+                doWatch: false,
+                isDeep: false,
+                individual: {
+                    doWatch: false,
+                    isDeep: false
                 }
             },
             center: {
